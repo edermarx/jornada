fdd1ea6544a3f2d82826a81b630b7454b443696a
Eder Marques <eder.marques@citi.org.br>
2019-07-26 16:58:58 -0300

    add express

--- node/app.js
+++ node/app.js
@@ -1,7 +1,21 @@
-const dateImport = require('./imports/date');
-const { myDate, otherDate, print: imprimir } = require('./imports/date');
+const express = require('express');
 
-console.log(dateImport);
+const app = express();
 
-console.log(myDate);
-dateImport.print(otherDate);
+const users = ['fejwfklew', 'fejfewwfklew', 'ew'];
+
+app.get('/', (req, res) => {
+  res.send('<h1>yay</h1>');
+});
+
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+app.get('/ip', (req, res) => {
+  res.send(`Seu ip é ${req.ip}`);
+});
+
+app.listen(3000, () => {
+  console.log('READY');
+});
