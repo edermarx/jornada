9a3da3c726be9cecb21d1ba0d9d2c4ad1020615d
Eder Marques <eder.marques@citi.org.br>
2019-07-26 13:22:12 -0300

    change letters for numbers

--- js/change-filename.js
+++ js/change-filename.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const path = require('path');
 
-const newFile = (file) => {
-  const num = +file.split('-')[0];
-  const letter = String.fromCharCode(num + 96);
-  return file.replace(num, 'a' + letter);
+const newFile = (file, i) => {
+  const letters = +file.split('-')[0];
+  console.log(letters - i);
+  // return file.replace((letters[0] === 'a' ?  '0' : letters[0] === 'b' ? '1' : letters[0]) + (+letters[1].charCodeAt() - 96));
 };
 
 fs.readdir(__dirname, (err, files) => {
-  files.forEach((file) => {
-    exec(`mv ${path.join(__dirname, file)} ${path.join(__dirname, newFile(file))}`);
+  files.forEach((file, i) => {
+    newFile(file, i);
+    // exec(`mv ${path.join(__dirname, file)} ${path.join(__dirname, newFile(file))}`);
   });
 });
